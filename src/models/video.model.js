72cca355d212bc161f1125ff583518ca2fc51194
k-ghale <kabinghale888@gmail.com>
2026-02-18 10:26:43 -0500

    user and video model with hooks and jwt

---
+++ src/models/video.model.js
@@ -0,0 +1,54 @@
+
+import mongoose, {Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        }
+        ,
+        title: {
+            type: String,
+            required: true
+        }
+        ,
+        description: {
+            type: String,
+            required: true
+        }
+        ,
+        duration: {
+            type: Number, // cloudnary url
+            required: true
+        }
+        ,
+        views : {
+            type: Number,
+            default: 0 
+        },
+        isPublished: {
+            type: Boolean,
+            default : true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamp: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+
+
+export const Video = new mongoose.model("Video", videoSchema)
